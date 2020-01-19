7d0629435875ddec40b50e08419033c0c1f59481
Edward Iga Kigongo <igakigongo@gmail.com>
2020-01-19 23:46:56 +0100

    Add a custom event with data - check CustomEvent.detail property

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function render(evt){
   console.log('DOM Content Loaded');
   console.log(evt);
 
+  let customEvent = new CustomEvent('customEvent', { detail: 30 });
   let event = new Event('build');
   let buttonDOMElement = document.querySelector('#buttonOne');
 
@@ -11,6 +12,7 @@ function render(evt){
 
     // Dispatch Event
     ev.target.dispatchEvent(event);
+    ev.target.dispatchEvent(customEvent);
 
     var objKind = (ev instanceof Event) ? "EventPrototype" : "ObjectPrototype";
     alert("We got a click event at " + ev.timeStamp + " with an argument object derived from: " + objKind );
@@ -20,6 +22,11 @@ function render(evt){
 
   // Listen for the event
   buttonDOMElement.addEventListener('build', function(evt){
+    console.log(evt);
+  });
+
+  // Listen for an event with Data
+  buttonDOMElement.addEventListener('customEvent', function(evt){
     console.log(evt);
   });
 }
