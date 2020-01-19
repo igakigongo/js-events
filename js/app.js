444473ba6958c1155d1b70605cb9b1add769e363
Edward Iga Kigongo <igakigongo@gmail.com>
2020-01-19 23:10:59 +0100

    Handle basic events - click event is used as an example

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 function render(evt){
   console.log('DOM Content Loaded');
   console.log(evt);
+  
+  let buttonDOMElement = document.querySelector('#buttonOne');
+
+  var example_click_handler = function (ev){
+
+    console.log(ev);
+
+    var objKind = (ev instanceof Event) ? "EventPrototype" : "ObjectPrototype";
+    alert("We got a click event at " + ev.timeStamp + " with an argument object derived from: " + objKind );
+  };
+   
+  buttonDOMElement.addEventListener('click', example_click_handler);
 }
 
 document.addEventListener('DOMContentLoaded', render);
